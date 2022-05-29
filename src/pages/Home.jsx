c080445e02e1d4c8c2d3ee27c909f79a253634b6
shaarangg <shaarangg.singh@gmail.com>
2022-05-29 13:34:24 +0530

    feat: Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,19 +4,18 @@ function Home() {
     return (
         <>
             <Navbar />
-            <div className="homeContainer">
-                <div className="title">Authentic, Distinctive, Convenient. </div>
-                <p className="content">
-                    Sernity.js aims to tackle the problem in social media platforms where it uses the power of Natural Language Processing to detect and correct racism as well as all forms of
-                    microaggression and foul criticism. It gives you the best developer experience by giving one call detection of hate speech in the input text.
-                </p>
-                <div className="btnContainer">
-                    <Link to="/docs" className="Docsbtn">
-                        Start Learning
-                    </Link>
-                    <Link to="/api" className="Apibtn">
-                        Test API
-                    </Link>
+            <div className="masterContainer">
+                <div className="homeContainer">
+                    <div className="title">Authentic, Distinctive, Convenient. </div>
+                    <p className="content">The feit aims at harnessing the power of Web3 enabled technologies to solve the issue of counterfeit products.</p>
+                    <div className="btnContainer">
+                        <Link to="/docs" className="Docsbtn">
+                            Customer Log In
+                        </Link>
+                        <Link to="/api" className="Apibtn">
+                            Company Log In
+                        </Link>
+                    </div>
                 </div>
             </div>
         </>
