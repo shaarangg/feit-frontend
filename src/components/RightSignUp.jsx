c630575c304c7e687ca1b34705fbd4803cfc8af8
shaarangg <shaarangg.singh@gmail.com>
2022-05-29 11:24:18 +0530

    feat: Register companies styling

--- src/components/RightSignUp.jsx
+++ src/components/RightSignUp.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { AiOutlineUser } from "react-icons/ai";
+import { GrOrganization } from "react-icons/gr";
+import { RiOrganizationChart } from "react-icons/ri";
+import { GiConfirmed } from "react-icons/gi";
 function RightSignUp() {
     const [user, setUser] = useState({
         cname: "",
@@ -14,59 +17,77 @@ function RightSignUp() {
             <div className="comp">
                 <div className="peak">
                     <div className="icontainer">
+                        <RiOrganizationChart />
+                    </div>
+                    <h3>Company Information</h3>
+                </div>
+                <div className="form">
+                    <div className="form-fields">
+                        <label htmlFor="cname">Company Name</label>
+                        <input type="text" name="cname" id="cname" onChange={(e) => setUser({ ...user, cname: e.target.value })} value={user.cname} />
+                    </div>
+                    <div className="form-fields">
+                        <label htmlFor="ctype">Type of Company</label>
+                        <select id="ctype" name="ctype" onChange={(e) => setUser({ ...user, ctype: e.target.value })} value={user.ctype}>
+                            <option>Apparel / Undergarments</option>
+                            <option>Education</option>
+                            <option>Electronics</option>
+                            <option>Entertainment</option>
+                            <option>Food And Beverage</option>
+                            <option>Footwear</option>
+                            <option>Pharmacy</option>
+                            <option>Retail(Apparel/Toys)</option>
+                            <option>Salon and Spa</option>
+                            <option>Sports and Fitness</option>
+                            <option>Stationery/Craft</option>
+                            <option>Super Market</option>
+                            <option>Toy Shops</option>
+                            <option>Other</option>
+                        </select>
+                    </div>
+                    <button>Next</button>
+                </div>
+            </div>
+            <div className="comp">
+                <div className="peak">
+                    <div className="icontainer">
                         <AiOutlineUser />
                     </div>
-                    <div>Company Information</div>
+                    <h3>Personal Information</h3>
                 </div>
                 <div className="form">
-                    <label htmlFor="cname">Company Name</label>
-                    <input type="text" name="cname" id="cname" onChange={(e) => setUser({ ...user, cname: e.target.value })} value={user.cname} />
-                    <label htmlFor="ctype">Type of Company</label>
-                    <select id="ctype" name="ctype" onChange={(e) => setUser({ ...user, ctype: e.target.value })} value={user.ctype}>
-                        <option>Apparel / Undergarments</option>
-                        <option>Education</option>
-                        <option>Electronics</option>
-                        <option>Entertainment</option>
-                        <option>Food And Beverage</option>
-                        <option>Footwear</option>
-                        <option>Pharmacy</option>
-                        <option>Retail(Apparel/Toys)</option>
-                        <option>Salon and Spa</option>
-                        <option>Sports and Fitness</option>
-                        <option>Stationery/Craft</option>
-                        <option>Super Market</option>
-                        <option>Toy Shops</option>
-                        <option>Other</option>
-                    </select>
+                    <div className="form-fields">
+                        <label htmlFor="ename">Name</label>
+                        <input type="text" name="ename" id="ename" onChange={(e) => setUser({ ...user, ename: e.target.value })} value={user.ename} />
+                    </div>
+                    <div className="form-fields">
+                        <label htmlFor="email">Email ID</label>
+                        <input type="email" name="email" id="email" onChange={(e) => setUser({ ...user, email: e.target.value })} value={user.email} />
+                    </div>
+                    <div className="form-fields">
+                        <label htmlFor="number">Mobile No.</label>
+                        <input type="tel" name="number" id="number" onChange={(e) => setUser({ ...user, mobile: e.target.value })} value={user.mobile} />
+                    </div>
                     <button>Next</button>
                 </div>
             </div>
             <div className="comp">
                 <div className="peak">
-                    <div>img</div>
-                    <div>Personal Information</div>
+                    <div className="icontainer">
+                        <GiConfirmed />
+                    </div>
+                    <h3>Confirm Account</h3>
                 </div>
                 <div className="form">
-                    <label htmlFor="ename">Name</label>
-                    <input type="text" name="ename" id="ename" onChange={(e) => setUser({ ...user, ename: e.target.value })} value={user.ename} />
-                    <label htmlFor="email">Email ID</label>
-                    <input type="email" name="email" id="email" onChange={(e) => setUser({ ...user, email: e.target.value })} value={user.email} />
-                    <label htmlFor="number">Mobile No.</label>
-                    <input type="tel" name="number" id="number" onChange={(e) => setUser({ ...user, mobile: e.target.value })} value={user.mobile} />
-                    <button>Next</button>
-                </div>
-            </div>
-            <div className="comp">
-                <div className="peak">
-                    <div>img</div>
-                    <div>Confirm Account</div>
-                </div>
-                <div className="form">
-                    <label htmlFor="otpe">Enter otp sent on email</label>
-                    <input type="number" name="otpe" id="otpe" />
-                    <label htmlFor="otpn">enter otp sent on email</label>
-                    <input type="number" name="otpn" id="otpn" />
-                    <button>Next</button>
+                    <div className="form-fields">
+                        <label htmlFor="otpe">Enter otp sent on email</label>
+                        <input type="number" name="otpe" id="otpe" />
+                    </div>
+                    <div className="form-fields">
+                        <label htmlFor="otpn">enter otp sent on email</label>
+                        <input type="number" name="otpn" id="otpn" />
+                        <button>Next</button>
+                    </div>
                 </div>
             </div>
         </div>
